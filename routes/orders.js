1dc6bf30e1e2cb7279e12361a90d317199f49c49
Sandeep Chopra <sandeep.chopra7@gmail.com>
2020-06-30 19:00:06 +0000

    works on routes

--- routes/orders.js
+++ routes/orders.js
@@ -3,12 +3,19 @@ const router = express.Router();
 
 const { notifyOwner } = require("../helpers/notifyOwner");
 
-module.exports = ({ getMenuItems, getCompletedOrder, placeOrder, addMenuItem }) => {
+module.exports = ({
+  getMenuItems,
+  getCompletedOrder,
+  placeOrder,
+  addMenuItem,
+}) => {
   // GET all orders
   // GET * FROM ORDERED_ITEMS TABLE
-  router.get("/:id", (req, res) => {
+  router.get("/new", (req, res) => {
     getMenuItems()
-      .then(menu => {
+      .then((menu) => {
+        console.log(menu);
+
         let templateVars = {
           orders: [1, 2, 3],
           menuItems: menu,
@@ -19,9 +26,10 @@ module.exports = ({ getMenuItems, getCompletedOrder, placeOrder, addMenuItem }) 
         res.status(500).json({ error: err.message });
       });
   });
+
   router.get("/:id/completed", (req, res) => {
     getCompletedOrder()
-      .then(completedOrder => {
+      .then((completedOrder) => {
         let templateVars = {
           completedOrder,
         };
@@ -32,28 +40,38 @@ module.exports = ({ getMenuItems, getCompletedOrder, placeOrder, addMenuItem }) 
       });
   });
   // POST - place an order
-  router.post("/:id", (req, res) => {
-  // { specialInstructions: '',
-  // orderDetails: '{"1":1,"3":1}' }
-  console.log('req.body: ', req.body)
+  router.post("/", (req, res) => {
+    // { specialInstructions: '',
+    // orderDetails: '{"1":1,"3":1}' }
+    console.log("req.body: ", req.body);
     console.log("creating a new order");
     //       (id, order_placed_at, special_instructions, order_ready_duration, order_ready, order_complete_at)
     const menuItems = JSON.parse(req.body.orderDetails);
+
     // Loop the menuItems object and call addMenuItem add each menu item to a new ordered_item
-    placeOrder(1, new Date(), req.body.specialInstructions, 30, false, null).then(menuItemId => {
-      // req.session.menu_item_id = menuItemId[0].id
-      // console.log('req.session: ', req.session)
-      for (const menuItem in menuItems) {
-        addMenuItem(menuItemId[0].id, Number(menuItem), 0, menuItems[menuItem]);
-      }
-    }).then(id => {
-      getCompletedOrder()
-        .then(completedOrder => {
-          console.log('completedOrder: ', completedOrder)
-          res.redirect(`/orders/1/completed`);
-        })
-        .catch(err => console.log(err));
-    });
+    placeOrder(1, new Date(), req.body.specialInstructions, 30, false, null)
+      .then((menuItemId) => {
+        // req.session.menu_item_id = menuItemId[0].id
+        // console.log('req.session: ', req.session)
+        for (const menuItem in menuItems) {
+          addMenuItem(
+            menuItemId[0].id,
+            Number(menuItem),
+            0,
+            menuItems[menuItem]
+          );
+        }
+      })
+      .then((id) => {
+        getCompletedOrder()
+          .then((completedOrder) => {
+            console.log("completedOrder: ", completedOrder);
+            res.redirect(`/orders/1`);
+          })
+          .catch((err) => console.log(err));
+      });
+
+    notifyOwner();
   });
   return router;
 };
